b57522ca42cc4500269bb55d3ad512b772c82cbf
Suvda-a <suvdaaalimaa@gmail.com>
2025-05-20 17:00:30 +0800

    test worked locally

--- app.py
+++ app.py
@@ -1,34 +1,33 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-data_store = {"book": "Modern C++", "phone": "iPhone 14"}
 
 @app.route('/lab15_16/operation/', methods=['POST'])
 def calculate():
-  data = request.get_json()
+  if not request.is_json:
+    return jsonify({'error': 'Unsupported Media Type'}), 415
 
+  data = request.get_json()
   a = data.get('a')
   b = data.get('b')
   operator = data.get('operator')
 
-  if a is None or operator not in ['+', '-', '*', '/']:
+  if a is None or b is None or operator not in ['+', '-', '*', '/']:
     return jsonify({'error': 'Invalid input'}), 400
-  
+
   try:
     if operator == '+':
-      result = a + b
+      result = int(a) + int(b)
     elif operator == '-':
-      result = a - b
+      result = int(a) - int(b)
     elif operator == '*':
-      result = a * b
+      result = int(a) * int(b)
     elif operator == '/':
-      result = a / b
-    else:
-      return jsonify({'error': 'unsupported operator'}), 400
-    return jsonify({'result: ': str(result)}), 200
-  
+      result = int(a) / int(b)
+    return jsonify({'result': str(result)}), 200
+
   except Exception as e:
-    return jsonify({'error': str(e)}), 500
+      return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-  app.run(debug = True)
+  app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,26 +7,26 @@ class TestApp(unittest.TestCase):
     self.app = app.test_client()
     self.app.testing = True
   
-  def Test_Case_1(self):
+  def test_case_1(self):
     payload = {"a": 5, "b": 6, "operator":"+"}
-    response = self.app.post('/lab15_16/operation/', data=json.dumps(payload), content_type='app')
+    response = self.app.post('/lab15_16/operation/', data=json.dumps(payload), content_type='application/json')
     self.assertIn(response.status_code, [200, 201])
     data = response.get_json()
     self.assertEqual(data['result'], '11')
   
-  def Test_Case_2(self):
+  def test_case_2(self):
     payload = {"a": 11, "b": 8, "operator":"-"}
-    response = self.app.post('/lab15_16/operation/', data=json.dumps(payload), content_type='app')
+    response = self.app.post('/lab15_16/operation/', data=json.dumps(payload), content_type='application/json')
     self.assertIn(response.status_code, [200, 201])
     data = response.get_json()
     self.assertEqual(data['result'], '3')
   
-  def Test_Case_3(self):
+  def test_case_3(self):
     payload = {"a": 2, "b": 2, "operator":"ddd"}
-    response = self.app.post('/lab15_16/operation/', data=json.dumps(payload), content_type='app')
-    self.assertIn(response.status_code, [200, 201])
+    response = self.app.post('/lab15_16/operation/', data=json.dumps(payload), content_type='application/json')
+    self.assertIn(response.status_code, [400, 422])
     data = response.get_json()
-    self.assertEqual(data['result'], 'unknown')  
+    self.assertEqual(data['error'], 'Invalid input')  
 
 if __name__ =='__main':
   unittest.main()
